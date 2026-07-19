b64ff8c2dd4e4742afbd7c9395a781c83d8e881e
its3sa7o-afk <its3sa.7o@gmail.com>
2026-07-19 11:00:58 +0300

    Update injector.lua

--- injector.lua
+++ injector.lua
@@ -1,2 +1,6 @@
--- Savannah Life Money Script
-local targetEventName = "AddMoney"
+-- Savannah LifeMoney Injector
+-- Optimized for Delta Executor
+
+local remoteName = "AddMoney" -- The target RemoteEvent identified
+local amountPerRequest = 1000 -- How much to request each time
+local loopSpeed = 0.1 -- Delay between requests (seconds)
