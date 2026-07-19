ec4464249e8a9f02116e5e6c55d9c33dee9ae500
its3sa7o-afk <its3sa.7o@gmail.com>
2026-07-19 10:59:01 +0300

    Update injector.lua

--- injector.lua
+++ injector.lua
@@ -1,8 +1,2 @@
--- [[ SAVANNAH LIFE - CUSTOM GUI PAYLOAD ]]
-
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Parent = game.CoreGui
-
-local Frame = Instance.new("Frame")
-Frame. 
-[ TARGET MAP ID: 18214855317 ] 
+-- Savannah Life Money Script
+local targetEventName = "AddMoney"
